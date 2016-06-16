9ed8dc74893769fc4a3f4519600e058e54d0c3ce
Simon Dingley <simon@prolificnotion.co.uk>
2016-06-16 15:27:58 +0100

    Adds a new IShipZone interface as a hook for later use, at the moment inherits directly from IShipCountry

---
+++ src/Merchello.Core/Models/Interfaces/IShipZone.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Merchello.Core.Models.Interfaces
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Globalization;
+	using System.Runtime.Serialization;
+
+	/// <summary>
+	/// An interface to use for any shipping zones which are used to group IShipCountry instances
+	/// </summary>
+	/// <seealso cref="Merchello.Core.Models.IShipCountry" />
+	public interface IShipZone { }
+}
