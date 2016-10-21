457ca35a85db3dce44ac290987489318efe4f31f
Simon Dingley <simon@prolificnotion.co.uk>
2016-10-21 15:32:45 +0100

    Adds new merchShipZone table and shipZone foreign key and bumps the version to 2.4.0

---
+++ src/Merchello.Core/Models/Rdbms/ShipZoneDto.cs
@@ -0,0 +1,72 @@
+﻿namespace Merchello.Core.Models.Rdbms
+{
+	using System;
+	using Umbraco.Core.Persistence;
+	using Umbraco.Core.Persistence.DatabaseAnnotations;
+
+	[TableName("merchShipZone")]
+    [PrimaryKey("pk", autoIncrement = false)]
+    [ExplicitColumns]
+    internal class ShipZoneDto
+    {
+		/// <summary>
+		/// Gets or sets the primary key.
+		/// </summary>
+		/// <value>
+		/// The primary key.
+		/// </value>
+		[Column("pk")]
+        [PrimaryKeyColumn(AutoIncrement = false)]
+        [Constraint(Default = "newid()")]
+        public Guid Key { get; set; }
+
+		/// <summary>
+		/// Gets or sets the catalog key.
+		/// </summary>
+		/// <value>
+		/// The catalog key.
+		/// </value>
+		[Column("catalogKey")]
+        [ForeignKey(typeof(WarehouseCatalogDto), Name = "FK_merchCatalogShipZone_merchWarehouseCatalog", Column = "pk")]
+        public Guid CatalogKey { get; set; }
+
+		/// <summary>
+		/// Gets or sets the zone code pretty much like a country code.
+		/// </summary>
+		/// <value>
+		/// The zone code.
+		/// </value>
+		[Column("zoneCode")]
+        public string ZoneCode { get; set; }
+
+		/// <summary>
+		/// Gets or sets the name of the shipping zone.
+		/// </summary>
+		/// <value>
+		/// The name of the shipping zone.
+		/// </value>
+		[Column("name")]
+        public string Name { get; set; }
+
+
+		/// <summary>
+		/// Gets or sets the update date.
+		/// </summary>
+		/// <value>
+		/// The update date.
+		/// </value>
+		[Column("updateDate")]
+        [Constraint(Default = "getdate()")]
+        public DateTime UpdateDate { get; set; }
+
+		/// <summary>
+		/// Gets or sets the create date.
+		/// </summary>
+		/// <value>
+		/// The create date.
+		/// </value>
+		[Column("createDate")]
+        [Constraint(Default = "getdate()")]
+        public DateTime CreateDate { get; set; }
+    }
+}
---
+++ src/Merchello.Core/Persistence/Migrations/Upgrades/TargetVersionTwoFourZero/CreateShipZoneTable.cs
@@ -0,0 +1,55 @@
+﻿namespace Merchello.Core.Persistence.Migrations.Upgrades.TargetVersionTwoFourZero
+{
+	using Merchello.Core.Configuration;
+	using Merchello.Core.Models.Rdbms;
+
+	using Umbraco.Core;
+	using Umbraco.Core.Logging;
+	using Umbraco.Core.Persistence;
+	using Umbraco.Core.Persistence.Migrations;
+
+	//using DatabaseSchemaHelper = Merchello.Core.Persistence.Migrations.DatabaseSchemaHelper;
+
+    /// <summary>
+    /// The create product collection table.
+    /// </summary>
+    [Migration("2.4.0", 1, MerchelloConfiguration.MerchelloMigrationName)]
+    public class CreateShipZoneTable : IMerchelloMigration
+    {
+        /// <summary>
+        /// The schema helper.
+        /// </summary>
+        private readonly DatabaseSchemaHelper _schemaHelper;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="CreateShipZoneTable"/> class.
+		/// </summary>
+		public CreateShipZoneTable()
+        {
+            var dbContext = ApplicationContext.Current.DatabaseContext;
+            this._schemaHelper = new DatabaseSchemaHelper(dbContext.Database, LoggerResolver.Current.Logger, dbContext.SqlSyntax);
+        }
+
+        /// <summary>
+        /// Adds the merchProductCollection table to the database.
+        /// </summary>
+        public void Up()
+        {
+            if (!this._schemaHelper.TableExist("merchShipZone"))
+            {
+                this._schemaHelper.CreateTable(false, typeof(ShipZoneDto));
+            }
+        }
+
+        /// <summary>
+        /// The down.
+        /// </summary>
+        /// <exception cref="DataLossException">
+        /// Throws a data loss exception if a downgrade is attempted
+        /// </exception>
+        public void Down()
+        {
+            throw new DataLossException("Cannot downgrade from a version 2.4.0 database to a prior version, the database schema has already been modified");
+        }
+    }
+}
